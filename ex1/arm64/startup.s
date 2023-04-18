fd46d6750d66ea15c2778d44ca0839c40ae003f8
Gabriel Fontes <hi@m7.rs>
2023-04-18 15:17:46 -0300

    ex1-arm64: fix comment errors

--- ex1/arm64/startup.s
+++ ex1/arm64/startup.s
@@ -3,8 +3,8 @@
 .global _start
 
 _start:
-    ldr x30, =stack_top	    # initial stack address
-    mov sp, x30             # set stack address
-    bl main                 # branch to main()
-    ldr x0, =0x84000008     # SYSTEM_OFF
-    hvc #0                  # hypervisor call
+    ldr x30, =stack_top     /* initial stack address */
+    mov sp, x30             /* set stack address     */
+    bl main                 /* branch to main()      */
+    ldr x0, =0x84000008     /* SYSTEM_OFF            */
+    hvc #0                  /* hypervisor call       */
